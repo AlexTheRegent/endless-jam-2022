91629f633a9ba5432a0b6359a8936f68bedd1ac6
AlexTheRegent <alextheregent@gmail.com>
2022-03-15 00:04:51 +0300

    Set asteroid  breakdown chance to 25%

--- prefabs/asteroid_big.gd
+++ prefabs/asteroid_big.gd
@@ -6,7 +6,7 @@ const SMALL_ASTEROID_ANGLE_SPREAD := 15.0
 
 
 func hit(bullet: Node2D) -> void:
-	if randf() < 1.25:
+	if randf() < 0.25:
 		get_node("/root/game").spawn_asteroid(global_position - Vector2(randf() * SMALL_ASTEROID_DISTANCE_SPREAD, randf() * SMALL_ASTEROID_DISTANCE_SPREAD), direction, "small")
 		get_node("/root/game").spawn_asteroid(global_position + Vector2(randf() * SMALL_ASTEROID_DISTANCE_SPREAD, randf() * SMALL_ASTEROID_DISTANCE_SPREAD), direction, "small")
 
