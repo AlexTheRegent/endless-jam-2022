e3e203f735c68b82f056075e7f71dd98e623b514
AlexTheRegent <alextheregent@gmail.com>
2022-03-16 01:14:51 +0300

    Added random direction rotation for big asteroids on split

--- prefabs/asteroid_big.gd
+++ prefabs/asteroid_big.gd
@@ -7,7 +7,12 @@ const SMALL_ASTEROID_ANGLE_SPREAD := 15.0
 
 func hit(bullet: Node2D) -> void:
 	if randf() < 0.25:
-		get_node("/root/game").spawn_asteroid(global_position - Vector2(randf() * SMALL_ASTEROID_DISTANCE_SPREAD, randf() * SMALL_ASTEROID_DISTANCE_SPREAD), direction, "small")
-		get_node("/root/game").spawn_asteroid(global_position + Vector2(randf() * SMALL_ASTEROID_DISTANCE_SPREAD, randf() * SMALL_ASTEROID_DISTANCE_SPREAD), direction, "small")
+		var spawn_pos_1 := global_position - Vector2(randf() * SMALL_ASTEROID_DISTANCE_SPREAD, randf() * SMALL_ASTEROID_DISTANCE_SPREAD)
+		var spawn_pos_2 := global_position + Vector2(randf() * SMALL_ASTEROID_DISTANCE_SPREAD, randf() * SMALL_ASTEROID_DISTANCE_SPREAD)
+		var spread_angle_1 := randf() * PI/8
+		var spread_angle_2 := -randf() * PI/8
+
+		get_node("/root/game").spawn_asteroid(spawn_pos_1, direction.rotated(spread_angle_1), "small")
+		get_node("/root/game").spawn_asteroid(spawn_pos_2, direction.rotated(spread_angle_2), "small")
 
 	.hit(bullet)
